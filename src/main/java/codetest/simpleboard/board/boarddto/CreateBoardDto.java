09ed4361a7f008702641e4bc21442824d30b501b
ktk8916 <ktk8916@naver.com>
2023-02-13 17:29:44 +0900

    [feat] : 게시글 작성용 dto 추가

---
+++ src/main/java/codetest/simpleboard/board/boarddto/CreateBoardDto.java
@@ -0,0 +1,13 @@
+package codetest.simpleboard.board.boarddto;
+
+import java.util.Date;
+
+public class CreateBoardDto {
+
+    private String title;
+    private String content;
+    private Long hits;
+    private String regName;
+    private Date regDttm;
+    
+}
