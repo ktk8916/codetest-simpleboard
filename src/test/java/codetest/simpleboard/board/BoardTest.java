2c50b634bf1e261938eb7d2b42cd9b7109424987
ktk8916 <ktk8916@naver.com>
2023-02-14 15:41:38 +0900

    [test] : board 생성 테스트 추가

---
+++ src/test/java/codetest/simpleboard/board/BoardTest.java
@@ -0,0 +1,20 @@
+package codetest.simpleboard.board;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.assertj.core.api.Assert;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BoardTest {
+    @Test
+    @DisplayName("새로 생성된 board의 조회수는 0이고, 생성일자는 null이 아니어야 한다")
+    void testCreateBoard() {
+        Board board = Board.createBoard("title", "content", "name");
+
+        assertEquals(board.getHits(), 0);
+        assertNotEquals(board.getRegDttm(), null);
+    }
+}
