1b4c2bafe2d1a0aaa9b3d2ed9c3cfc844460a3b3
ktk8916 <ktk8916@naver.com>
2023-02-15 11:23:05 +0900

    [test] : 게시글 조회 시 조회수 증가 테스트 추가

--- src/test/java/codetest/simpleboard/board/BoardServiceTest.java
+++ src/test/java/codetest/simpleboard/board/BoardServiceTest.java
@@ -1,24 +1,38 @@
 package codetest.simpleboard.board;
 
 import codetest.simpleboard.board.boarddto.BoardDetailViewDto;
+import codetest.simpleboard.board.boarddto.WriteBoardDto;
+
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@Transactional
 class BoardServiceTest {
 
     @Autowired BoardService boardService;
+
+    @BeforeEach
+    public void init(){
+        boardService.writeBoard(new WriteBoardDto("title1", "content1", "nam1"));
+        boardService.writeBoard(new WriteBoardDto("title2", "content2", "name2"));
+        boardService.writeBoard(new WriteBoardDto("title3", "content3", "name3"));
+    }
     
     @Test
-    @DisplayName("조회된 게시글은 조회수가 증가해야 한다")
+    @DisplayName("게시글 조회 기능은 조회수를 증가시켜야 한다.")
     public void t1(){
-        Board board = Board.createBoard("title", "content", "name");
+        Long beforeHits = boardService.findOne(1L).getHits();
 
-        BoardDetailViewDto viewBoard = boardService.detailViewBoard( ??? ); //조회를 어떻게 하지 ??
+        BoardDetailViewDto viewBoard = boardService.detailViewBoard( 1L );
 
-        assertEquals(viewBoard.getHits(), 1);
+        assertEquals(beforeHits+ 1, viewBoard.getHits());
     }
 }
