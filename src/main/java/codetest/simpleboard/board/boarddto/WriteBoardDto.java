a4f06e875cd98926e2cb7b2a0666b7dfca9280fc
ktk8916 <ktk8916@naver.com>
2023-02-15 11:22:41 +0900

    [feat] : 생성자 추가

--- src/main/java/codetest/simpleboard/board/boarddto/WriteBoardDto.java
+++ src/main/java/codetest/simpleboard/board/boarddto/WriteBoardDto.java
@@ -2,9 +2,11 @@ package codetest.simpleboard.board.boarddto;
 
 import javax.validation.constraints.NotBlank;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class WriteBoardDto {
     @NotBlank
     private String title;
