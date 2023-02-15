47a92a1de848e9bdb0e8030e302db4e3c2174db6
ktk8916 <ktk8916@naver.com>
2023-02-15 13:01:42 +0900

    [refactor] : 기본 생성자 추가

--- src/main/java/codetest/simpleboard/board/boarddto/WriteBoardDto.java
+++ src/main/java/codetest/simpleboard/board/boarddto/WriteBoardDto.java
@@ -4,9 +4,11 @@ import javax.validation.constraints.NotBlank;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class WriteBoardDto {
     @NotBlank
     private String title;
